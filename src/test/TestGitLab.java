a55f13c744658417bdd7dcc48ed50589c9b6854a
John <317horus@gmail.com>
2016-11-28 15:16:01 -0500

    add cruel

--- src/test/TestGitLab.java
+++ src/test/TestGitLab.java
@@ -4,7 +4,7 @@ public class TestGitLab {
 	
 	public static void Main (String [] args) {
 	
-	System.out.println("hello beautiful world");
+	System.out.println("hello beautiful cruel world");
 
 }
 }
